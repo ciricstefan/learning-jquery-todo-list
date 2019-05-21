078b00f0c78c3a6e1588d3c85c7e3d13874b3476
ciricstefan <ciric.stefan95@gmail.com>
2019-05-21 13:19:23 +0200

    Remove function
    Added remove function to span
    Fixed event bubbling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $("li").click(function(){
 	$(this).toggleClass("checked");
 });
+
+// Click on X to delete Todo
+$("span").click(function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
